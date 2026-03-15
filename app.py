ecddb7aa4d02458404dd85e44f9b7b010ec1ac70
Livia <liviako@hotmail.com>
2026-03-15 15:20:09 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,30 @@
+{
+    "files.autoSave": "afterDelay",
+    "workbench.colorTheme": "Aura Dracula Spirit",
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    "editor.wordWrap": "on",
+    "jdk.telemetry.enabled": true,
+    "chat.viewSessions.orientation": "stacked",
+    "explorer.confirmDelete": false,
+    "explorer.confirmDragAndDrop": false,
+    "security.workspace.trust.untrustedFiles": "open",
+    "gitlens.ai.model": "vscode",
+    "gitlens.ai.vscode.model": "copilot:gpt-4.1",
+
+    "terminal.integrated.profiles": {
+        "PowerShell": {
+            "path": "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe",
+            "icon": "terminal-powershell"
+        },
+        "Command Prompt": {
+            "path": "C:\\Windows\\System32\\cmd.exe",
+            "icon": "terminal-cmd"
+        },
+        "Git Bash": {
+            "path": "C:\\Program Files\\Git\\bin\\bash.exe",
+            "icon": "terminal-git-bash"
+        }
+    },
+
+    "terminal.integrated.defaultProfile.windows": "Git Bash"
+}
